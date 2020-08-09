51e8414233d1f61dac9f1d89e042af74ab48e87c
feti <fkarakoc02@gmail.com>
2020-08-09 16:17:37 +0100

    the 3rd commit from local master

--- src/package2/FirstClass.java
+++ src/package2/FirstClass.java
@@ -6,6 +6,8 @@ public class FirstClass {
 		
 		System.out.println("This is the first changes from the local master");
 		System.out.println("This is the second commit from the local master");
+		System.out.println("This is the third commit from the local master");
+
 	}
 
 }
